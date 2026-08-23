f6a8099d1042f3213d5e08ab39e8afb43ec1f388
Indraaai <iindra09323@gmail.com>
2026-08-23 13:31:58 +0700

    constan

---
+++ const.go
@@ -0,0 +1,10 @@
+// varaibel const adalah data yang tidak bisa diubah lagi isisnya
+
+package main
+
+func main() {
+	const pi float64 = 3.14
+	println(pi)
+
+	// contant jika tidak digunakan tidak akan eror, berbeda dengan variabel yang harus digunakan
+}
