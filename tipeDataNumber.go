ee10b38faa375010afd8c4fcb1885cde533fcf06
Indraaai <iindra09323@gmail.com>
2026-08-23 13:12:19 +0700

    tipe data number

---
+++ tipeDataNumber.go
@@ -0,0 +1,13 @@
+// tipe data number integer //
+
+// tipe data number float //
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World! = ini adalah tipe  data string")
+	fmt.Println(1000, " = ini adalah tipe data integer")
+	fmt.Println(3.14, " = ini adalah tipe data float")
+}
