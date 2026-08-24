ab2922a6c48c490606cee5c6fc293d23cfb5358a
Indraaai <iindra09323@gmail.com>
2026-08-24 12:35:26 +0700

    function

---
+++ function.go
@@ -0,0 +1,15 @@
+// function di golang
+
+package main
+
+import "fmt"
+
+// contoh sebah function sederhana
+func sayHello() {
+	fmt.Println("Hello, World!")
+}
+
+// memanggil function sayHello di dalam function main
+func main() {
+	sayHello()
+}
